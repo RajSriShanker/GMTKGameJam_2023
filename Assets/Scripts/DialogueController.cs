831e90a961f080bcce76ee15b4a997fe570c8526
Raj Sri Shanker <raj.ss166@gmail.com>
2023-07-09 10:40:02 -0400

    Updated Main Menu UI & Dialogue Changes

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -9,31 +9,50 @@ public class DialogueController : MonoBehaviour
     [SerializeField] private TextMeshPro crowdText;
 
     string[] dialogueArray;
-    string crowdFilePath;
+    string crowdHitFilePath;
+    string crowdDodgeFilePath;
     
     // Start is called before the first frame update
     void Start()
-    {
-        crowdFilePath = Application.dataPath + "/Scripts/crowd.txt";
+    { 
+        crowdHitFilePath = Application.dataPath + "/Scripts/crowdHit.txt";
+        crowdDodgeFilePath = Application.dataPath + "/Scripts/crowdDodge.txt";
     }
 
     // Update is called once per frame
     void Update()
     {
         //Every time I press the space key, read one line from text file
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            dialogueArray = File.ReadAllLines(crowdFilePath);
-            crowdText.text = dialogueArray[Random.Range(0, dialogueArray.Length)];
+            CrowdHitDialogue();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            CrowdDodgeDialogue();
         }
     }
 
-    public void ReadFromFile()
+    public void ReadFromFile(string filePath)
     {
-        dialogueArray = File.ReadAllLines(crowdFilePath);
+        dialogueArray = File.ReadAllLines(filePath);
         foreach (string line in dialogueArray)
         {
             Debug.Log(line);
         }
     }
+
+    public void CrowdHitDialogue()
+    {
+        dialogueArray = File.ReadAllLines(crowdHitFilePath);
+        crowdText.text = dialogueArray[Random.Range(0, dialogueArray.Length)];
+    }
+
+    public void CrowdDodgeDialogue()
+    {
+        dialogueArray = File.ReadAllLines(crowdDodgeFilePath);
+        crowdText.text = dialogueArray[Random.Range(0, dialogueArray.Length)];
+    }
+
 }
