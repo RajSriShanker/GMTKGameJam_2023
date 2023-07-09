fef939cbf63ab36fa6f7a418f731850c0844de09
Jonathan Beegan <jwbeegan@gmail.com>
2023-07-09 11:19:05 -0400

    add sound playing

--- Assets/Sound/FMODPlayer.cs
+++ Assets/Sound/FMODPlayer.cs
@@ -6,43 +6,9 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class FMODPlayer : MonoBehaviour
-
-
-
 {
-    // Start is called before the first frame update
-    void Start()
 
 
-    {
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Red", transform.position); //red one shot
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Blue", transform.position); //blue one shot
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Green", transform.position); //green blue shot
-
-        //player dodges
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Dodge");
-
-        //player hurt
-        FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerHurt");
-
-        //placeholder combo sfx (working on a better one)
-        FMODUnity.RuntimeManager.PlayOneShot("event:/Combo");
-
-        //cheers for when combo is successful
-        FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdCheer-oneshots");
-
-        //boos for when combo fails
-        FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdBoo-oneshots");
-
-    }
-
-    
-  
-
-
-
-
-    //for the crowd ambience loop, you can just attack this part of the script to the crowd game object in the scene or keep it somewhere
     public FMOD.Studio.EventInstance instance;
 
     public FMODPlayer(EventInstance instance)
@@ -50,6 +16,80 @@ public class FMODPlayer : MonoBehaviour
         this.instance = FMODUnity.RuntimeManager.CreateInstance("event:/CrowdAmbeince");
         instance.start();
     }
-} 
+
+    //void Start()
+    //{
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/Red", transform.position); //red one shot
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/Blue", transform.position); //blue one shot
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/Green", transform.position); //green blue shot
+
+    //    //player dodges
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/Dodge");
+
+    //    //player hurt
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerHurt");
+
+    //    //placeholder combo sfx (working on a better one)
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/Combo");
+
+    //    //cheers for when combo is successful
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdCheer-oneshots");
+
+    //    //boos for when combo fails
+    //    FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdBoo-oneshots");
+
+    public void PlayRedShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Red", transform.position);
+    }
+
+    public void PlayBlueShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Blue", transform.position);
+    }
+
+    public void PlayGreenShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Green", transform.position);
+    }
+
+    public void PlayDodge()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Dodge");
+    }
+
+    public void PlayHurt()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/PlayerHurt");
+    }
+
+    public void PlayCombo()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Combo");
+    }
+
+    public void PlayCheer()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdCheer-oneshots");
+    }
+
+    public void PlayBoo()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/CrowdBoo-oneshots");
+
+    }
+
+    public void StopBoo()
+    {
+
+    }
+
+
+}
+    
+  
+
+
+
 
 
